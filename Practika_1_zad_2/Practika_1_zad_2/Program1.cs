4026cd075634765795906cc5622049769ef3a10d
Morok0 <95218742+Morok0@users.noreply.github.com>
2021-12-01 22:48:36 +0300

    Add files via upload

---
+++ Practika_1_zad_2/Practika_1_zad_2/Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace практика_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("введите целое число: ");
+            a = int.Parse(Console.ReadLine());
+            if (a % 2 == 1)
+            {
+                Console.WriteLine("число нечетное");
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
